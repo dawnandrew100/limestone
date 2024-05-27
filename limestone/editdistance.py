21dd12a0dc61823d639aebd6a24c5c173ade1f4d
Andrew Hennis <dawnandrew100@gmail.com>
2024-05-27 00:12:52 -0400

    Began implementation of Hirschberg algorithm

--- limestone/editdistance.py
+++ limestone/editdistance.py
@@ -6,7 +6,8 @@ except ImportError:
     numpy = None  
 
 def main():
-    print(smithWaterman.align("AGTACGCA","TATGC"))
+    print(hirschbergDist.align("AGTACGCA","TATGC"))
+    print(needlemanWunsch.align("AGTACGCA","TATGC"))
 
 def ljustlist(sequence: list[str], n: int, fillvalue='')->list[str]:
   return sequence + [fillvalue] * (n - len(sequence)) 
@@ -20,11 +21,11 @@ class _BASE():
       sequences = [querySequence,subjectSequence]
       return max(map(len, sequences))
 
+class _GLOBALBASE(_BASE):  
   def scoreMatrix(self, querySequence: str|list[str], subjectSequence: str|list[str])->list[list[int]]:
     matrix, _ = self(querySequence, subjectSequence)
     return matrix
-
-class _GLOBALBASE(_BASE):  
+  
   def distance(self, querySequence: str|list[str], subjectSequence: str|list[str])->int:
     matrix, _ = self(querySequence, subjectSequence)
     return matrix[matrix.shape[0]-1,matrix.shape[1]-1]
@@ -291,6 +292,102 @@ class waterman_smith_beyer(_GLOBALBASE):
   
       return self.alignment_score, self.pointer
 
+class hirschberg(_GLOBALBASE):
+    def __init__(self, match_score: int = 1, mismatch_penalty: int = -1, gap_penalty: int = -2):
+        self.match_score = match_score
+        self.mismatch_penalty = mismatch_penalty
+        self.gap_penalty = gap_penalty
+    
+    def __call__(self, querySequence, subjectSequence):
+        if isinstance(querySequence, str):
+            querySequence = querySequence.upper()
+            subjectSequence = subjectSequence.upper()
+        
+        if len(querySequence) == 0:
+            return '-' * len(subjectSequence), subjectSequence
+        elif len(subjectSequence) == 0:
+            return querySequence, '-' * len(querySequence)
+        elif len(querySequence) == 1 or len(subjectSequence) == 1:
+            return self._nw_align(querySequence, subjectSequence)
+        else:
+            xlen = len(querySequence)
+            xmid = xlen // 2
+            score_l = self._nw_score(querySequence[:xmid], subjectSequence)
+            score_r = self._nw_score(querySequence[xmid:][::-1], subjectSequence[::-1])[::-1]
+            ymid = numpy.argmax(score_l + score_r)
+
+            A_left, B_left = self(querySequence[:xmid], subjectSequence[:ymid])
+            A_right, B_right = self(querySequence[xmid:], subjectSequence[ymid:])
+            return A_left + A_right, B_left + B_right
+
+    def _nw_score(self, querySequence, subjectSequence):
+        m, n = len(querySequence), len(subjectSequence)
+        score = numpy.zeros((2, n + 1))
+
+        for j in range(1, n + 1):
+            score[0][j] = score[0][j - 1] + self.gap_penalty
+
+        for i in range(1, m + 1):
+            score[1][0] = score[0][0] + self.gap_penalty
+            for j in range(1, n + 1):
+                match = score[0][j - 1] + (self.match_score if querySequence[i - 1] == subjectSequence[j - 1] else self.mismatch_penalty)
+                delete = score[0][j] + self.gap_penalty
+                insert = score[1][j - 1] + self.gap_penalty
+                score[1][j] = max(match, delete, insert)
+            score[0] = score[1]
+
+        return score[1]
+
+    def _nw_align(self, querySequence, subjectSequence):
+        m, n = len(querySequence), len(subjectSequence)
+        score = numpy.zeros((m + 1, n + 1))
+        pointer = numpy.zeros((m + 1, n + 1))
+
+        for i in range(1, m + 1):
+            score[i][0] = score[i - 1][0] + self.gap_penalty
+            pointer[i][0] = 1
+        for j in range(1, n + 1):
+            score[0][j] = score[0][j - 1] + self.gap_penalty
+            pointer[0][j] = 2
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                match = score[i - 1][j - 1] + (self.match_score if querySequence[i - 1] == subjectSequence[j - 1] else self.mismatch_penalty)
+                delete = score[i - 1][j] + self.gap_penalty
+                insert = score[i][j - 1] + self.gap_penalty
+                score[i][j] = max(match, delete, insert)
+
+                if score[i][j] == match:
+                    pointer[i][j] = 3
+                elif score[i][j] == delete:
+                    pointer[i][j] = 1
+                else:
+                    pointer[i][j] = 2
+
+        queryAlign, subjectAlign = "", ""
+        i, j = m, n
+
+        while i > 0 or j > 0:
+            if pointer[i][j] == 3:
+                queryAlign = querySequence[i - 1] + queryAlign
+                subjectAlign = subjectSequence[j - 1] + subjectAlign
+                i -= 1
+                j -= 1
+            elif pointer[i][j] == 1:
+                queryAlign = querySequence[i - 1] + queryAlign
+                subjectAlign = '-' + subjectAlign
+                i -= 1
+            else:
+                queryAlign = '-' + queryAlign
+                subjectAlign = subjectSequence[j - 1] + subjectAlign
+                j -= 1
+
+        return queryAlign, subjectAlign
+
+    def align(self, querySequence: str | list[str], subjectSequence: str | list[str]) -> str:
+        aligned_query, aligned_subject = self(querySequence, subjectSequence)
+        return f"{aligned_query}\n{aligned_subject}"
+
 def frontWhiteSpace(querySequence: list[str], subjectSequence: list[str])->tuple[list[str],list[str]]: 
     #adds leading white space so that matrix works
     subjectSequence = rjustlist(subjectSequence)
@@ -302,7 +399,7 @@ needlemanWunsch = needleman_wunsch()
 watermanSmithBeyer = waterman_smith_beyer()
 smithWaterman = smith_waterman()
 levenshteinDist = levenshtein()
-#hirschbergDist = hirschberg()
+hirschbergDist = hirschberg()
 
 if __name__ == "__main__":
     main()
