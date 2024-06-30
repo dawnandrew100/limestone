8ca753c9cb86e125b809fc288071b8da5b9d31b0
Andrew Hennis <dawnandrew100@gmail.com>
2024-06-30 09:57:41 -0400

    Added new tests for Hirschberg

--- tests/test_Hirschberg.py
+++ tests/test_Hirschberg.py
@@ -5,9 +5,24 @@ from limestone import editdistance
 
 class TestHirschberg(unittest.TestCase):
         
-    def test_align(self):
+    def test_align1(self):
         nwalignment = editdistance.needlemanWunsch.align("BA", "ABA")
         hsalignment = editdistance.hirschbergDist.align("BA","ABA")
+        self.assertEqual(nwalignment, hsalignment)
+    
+    def test_align2(self):
+        nwalignment = editdistance.needlemanWunsch.align("ATCG","ATCG")
+        hsalignment = editdistance.hirschbergDist.align("ATCG","ATCG")
+        self.assertEqual(nwalignment, hsalignment)
+
+    def test_align3(self):
+        nwalignment = editdistance.needlemanWunsch.align("ACTG","FHYU")
+        hsalignment = editdistance.hirschbergDist.align("ACTG","FHYU")
+        self.assertEqual(nwalignment, hsalignment)
+
+    def test_align4(self):
+        nwalignment = editdistance.needlemanWunsch.align("AGTACGCA","TATGC")
+        hsalignment = editdistance.hirschbergDist.align("AGTACGCA","TATGC")
         self.assertEqual(nwalignment, hsalignment)
 
 if __name__ == '__main__':
