23d18eea8cd6188f8b23767bc4363abf989e1ec7
Andrew Hennis <dawnandrew100@gmail.com>
2024-11-15 11:38:55 -0500

    updating names to avoid naming conflict

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(
-    name='limestone',
+    name='goombay',
     version='0.13.0',
 
-    packages=['limestone'],
+    packages=['goombay'],
     package_data={'': ['*.py']},
     python_requires='>=3.9',
 
-    url='https://github.com/dawnandrew100/limestone'
+    url='https://github.com/dawnandrew100/goombay'
 )
